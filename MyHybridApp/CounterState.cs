494613b550768a9a81c1388280fac78661f7aa53
entvickler <entvickler@gmail.com>
2021-01-31 10:32:23 +0400

    Dark Commit

---
+++ MyHybridApp/CounterState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyHybridApp
+{
+    internal class CounterState
+    {
+        public int CurrentCount { get; private set; }
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+            StateChanged?.Invoke();
+        }
+
+        public event Action StateChanged;
+    }
+}
